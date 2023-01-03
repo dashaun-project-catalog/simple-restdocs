5e71de3ac340065f271797807859058d02126abb
dashaun <dashaun@dashaun.com>
2023-01-02 19:33:19 -0600

    fix doc

--- src/main/java/dev/dashaun/rest/simple/simplerest/DemoApplication.java
+++ src/main/java/dev/dashaun/rest/simple/simplerest/DemoApplication.java
@@ -2,17 +2,27 @@ package dev.dashaun.rest.simple.simplerest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+
+		Properties properties = new Properties();
+		properties.setProperty("spring.resources.static-locations",
+				"classpath:/static/docs/");
+		app.setDefaultProperties(properties);
+		app.run(args);
 	}
 
 }
--- src/test/java/dev/dashaun/rest/simple/simplerest/DemoApplicationTests.java
+++ src/test/java/dev/dashaun/rest/simple/simplerest/DemoApplicationTests.java
@@ -1,47 +1,44 @@
 package dev.dashaun.rest.simple.simplerest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-// import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-// import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
-// import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-// import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
-// import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
-
-@AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @WebMvcTest(GreetingController.class)
-@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 class WebLayerTest {
 
-	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@BeforeEach
+	void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.apply(documentationConfiguration(restDocumentation))
+				.build();
+	}
 
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
